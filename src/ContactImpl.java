d1f3e3d0c40d2bc7671273e79d60301ea4d57c18
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-01-28 14:25:13 +0000

    added variables and empty methods

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -1,3 +1,29 @@
 public class ContactImpl implements Contact
 {
+    private int Id;
+    private String name;
+    private String notes;
+
+    public ContactImpl(int Id, String name)
+    {
+        this.name = name;
+        this.Id = Id;
+    }
+
+    public int getId()
+    {
+    }
+
+    public String getName()
+    {
+    }
+
+    public String getNotes()
+    {
+    }
+
+    public void addNotes(String note)
+    {
+
+    }
 }
