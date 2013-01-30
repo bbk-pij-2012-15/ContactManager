1683f1008bc94cd1c80a7d4da3618470ca9db9e2
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-01-30 14:25:09 +0000

    temporarily set notes to protected, to run test
    (test failed as expected)

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -2,7 +2,7 @@ public class ContactImpl implements Contact
 {
     private int Id;
     private String name;
-    private String notes;
+    protected String notes;
 
     public ContactImpl(int Id, String name)
     {
