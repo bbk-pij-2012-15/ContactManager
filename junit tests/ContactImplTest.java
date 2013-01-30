a73010f7bcfe24fbad017f3e2fd44f24a92df489
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-01-30 14:24:13 +0000

    written initial test to fail

--- junit tests/ContactImplTest.java
+++ junit tests/ContactImplTest.java
@@ -3,39 +3,55 @@ import static org.junit.Assert.*;
 
 public class ContactImplTest
 {
+
+    // create a ContactImpl for the entire test to use
+    // n.b. test WILL fail at this point as ContactImpl not yet implemented
+    public ContactImpl cont = new ContactImpl(001, "Test Contact");
+
     @Before
     public void buildUp() throws Exception
     {
-        fail("not yet written");
+       fail("not yet written");
     }
 
     @After
     public void cleanUp() throws Exception
     {
-        fail("not yet written");
+        // remove object after test by setting it to null
+        // just in case it interferes with future tests
+        cont = null;
     }
 
     @Test
     public void testGetId() throws Exception
     {
-        fail("not yet written");
+        int output = cont.getId();
+        int expected = 001;
+        assertEquals(expected, output);
     }
 
     @Test
     public void testGetName() throws Exception
     {
-        fail("not yet written");
-    }
-
-    @Test
-    public void testGetNotes() throws Exception
-    {
-        fail("not yet written");
+        String output = cont.getName();
+        String expected = "Test Contact";
+        assertEquals(expected, output);
     }
 
     @Test
     public void testAddNotes() throws Exception
     {
-        fail("not yet written");
+        // test AddNotes by making sure a note has been added and so
+        // it is not null. we test the actual String in next method
+        cont.addNotes("This is a note");
+        assertNotNull(cont.notes);
+    }
+
+    @Test
+    public void testGetNotes() throws Exception
+    {
+        String output = cont.getNotes();
+        String expected = "This is a note";
+        assertEquals(expected, output);
     }
 }
