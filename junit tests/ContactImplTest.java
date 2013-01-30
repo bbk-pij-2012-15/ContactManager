0325c70e86d187a50051d2e72a44b3aa9998c328
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-01-30 13:59:20 +0000

    wrote barebones test to fail, before starting to implement ContactImpl

---
+++ junit tests/ContactImplTest.java
@@ -0,0 +1,41 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactImplTest
+{
+    @Before
+    public void buildUp() throws Exception
+    {
+        fail("not yet written");
+    }
+
+    @After
+    public void cleanUp() throws Exception
+    {
+        fail("not yet written");
+    }
+
+    @Test
+    public void testGetId() throws Exception
+    {
+        fail("not yet written");
+    }
+
+    @Test
+    public void testGetName() throws Exception
+    {
+        fail("not yet written");
+    }
+
+    @Test
+    public void testGetNotes() throws Exception
+    {
+        fail("not yet written");
+    }
+
+    @Test
+    public void testAddNotes() throws Exception
+    {
+        fail("not yet written");
+    }
+}
