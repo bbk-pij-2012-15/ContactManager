cb9721724656d7eae8771736d4ffc3e56ba7a857
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-06 14:35:03 +0000

    written test to fail as ContactManager not implemented

---
+++ junit tests/ContactManagerImplTest.java
@@ -0,0 +1,89 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactManagerImplTest
+{
+    @After
+    public void cleanUp()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testAddFutureMeeting()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetPastMeeting()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetFutureMeeting()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetMeeting()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetFutureMeetingListContact()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetFutureMeetingListCalendar()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetPastMeetingList()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testAddNewPastMeeting()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testAddMeetingNotes()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testAddNewContact()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetContactsInt()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testGetContactsString()
+    {
+        fail("not written yet");
+    }
+
+    @Test
+    public void testFlush()
+    {
+        fail("not written yet");
+    }
+}
