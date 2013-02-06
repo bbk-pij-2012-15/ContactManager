639b317b5656bf95406357a0e169d9087d515e8e
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-06 15:43:48 +0000

    added ArrayList for contacts, and method for IdHelper to use

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -1,70 +1,80 @@
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 
 public class ContactManagerImpl implements ContactManager
 {
-    public int addFutureMeeting(Set<Contact> contacts, Calendar date);
+
+    private static List<ContactImpl> ContactArray = new ArrayList<ContactImpl>(1);
+
+    public static int getArraySize()
+    {
+        int size = ContactArray.size();
+        return size;
+    }
+
+    public int addFutureMeeting(Set<Contact> contacts, Calendar date)
     {
 
     }
 
-    public PastMeeting getPastMeeting(int id);
+    public PastMeeting getPastMeeting(int id)
     {
 
     }
 
-    public FutureMeeting getFutureMeeting(int id);
+    public FutureMeeting getFutureMeeting(int id)
     {
 
     }
 
-    public Meeting getMeeting(int id);
+    public Meeting getMeeting(int id)
     {
 
     }
 
-    public List<Meeting> getFutureMeetingList(Contact contact);
+    public List<Meeting> getFutureMeetingList(Contact contact)
     {
 
     }
 
-    public List<Meeting> getFutureMeetingList(Calendar date);
+    public List<Meeting> getFutureMeetingList(Calendar date)
     {
 
     }
 
-    public List<PastMeeting> getPastMeetingList(Contact contact);
+    public List<PastMeeting> getPastMeetingList(Contact contact)
     {
 
     }
 
-    public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text);
+    public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text)
     {
 
     }
 
-    public void addMeetingNotes(int id, String text);
+    public void addMeetingNotes(int id, String text)
     {
 
     }
 
-    public void addNewContact(String name, String notes);
+    public void addNewContact(String name, String notes)
     {
 
     }
 
-    public Set<Contact> getContacts(int... ids);
+    public Set<Contact> getContacts(int... ids)
     {
 
     }
 
-    public Set<Contact> getContacts(String name);
+    public Set<Contact> getContacts(String name)
     {
 
     }
 
-    public void flush();
+    public void flush()
     {
 
     }
