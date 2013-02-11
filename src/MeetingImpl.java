c1c3d34ef4e32a7ff07070c103a877a7b69d6602
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-11 14:48:24 +0000

    written empty class

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -1,3 +1,29 @@
-public class MeetingImpl
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MeetingImpl implements Meeting
 {
+    private int meetingId;
+    private Set<Contact> contactSet = new HashSet<Contact>();
+    private Calendar meetingCal = new GregorianCalendar().getInstance();
+
+    public MeetingImpl()
+    {
+
+    }
+
+    public int getId()
+    {
+    }
+
+    public Calendar getDate()
+    {
+    }
+
+    public Set<Contact> getContacts()
+    {
+    }
+
 }
