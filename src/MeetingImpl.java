0d66c81dd92bdd3d3a89c8d25fd713557ee4b888
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-11 16:21:49 +0000

    implemented 2 methods

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -1,29 +1,52 @@
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MeetingImpl implements Meeting
 {
     private int meetingId;
     private Set<Contact> contactSet = new HashSet<Contact>();
-    private Calendar meetingCal = new GregorianCalendar().getInstance();
+    private Calendar meetingCal;
 
-    public MeetingImpl()
+    public MeetingImpl(Set<Contact> set)
     {
-
+        this.meetingId = (set.size() + 1);
+        this.contactSet.addAll(set);
+        this.meetingCal = new GregorianCalendar().getInstance();
     }
 
     public int getId()
     {
+        return this.meetingId;
     }
 
     public Calendar getDate()
     {
+        return this.meetingCal;
     }
 
     public Set<Contact> getContacts()
     {
+        return this.contactSet;
+    }
+
+    public String getSetInfo()
+    {
+        String setInfo = "";
+        for (Iterator<Contact> itr = this.contactSet.iterator(); itr.hasNext(); )
+        {
+            Contact tmp = itr.next();
+            setInfo += tmp.getInfo();   // will implement this method later
+        }
+        return setInfo;
+    }
+
+    public String getMeetingInfo()
+    {
+        String id = "Meeting Id: " + this.meetingId;
+        String contacts = "Contacts at Meeting: " + this.getSetInfo();
+        String date = "Date of Meeting: " + this.meetingCal.get(GregorianCalendar.DAY_OF_MONTH) + "/" +
+                (this.meetingCal.get(GregorianCalendar.MONTH) + 1) + "/" + this.meetingCal.get(GregorianCalendar.YEAR);
+        String info = (id + "\n" + contacts + "\n" + date);
+        return info;
     }
 
 }
