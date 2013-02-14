37b3381fc5ea70479fe7513b8db89be76edf1de7
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-14 16:17:18 +0000

    added required methods to satisfy implements and extends

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -1,3 +1,32 @@
-public class PastMeetingImpl
+import java.util.Calendar;
+import java.util.Set;
+
+public class PastMeetingImpl implements PastMeeting extends Meeting
 {
+    private String meetingNotes;
+
+    public PastMeetingImpl(Set<Contact> set, Calendar date)
+    {
+          this.
+    }
+
+    public String getNotes()
+    {
+
+    }
+
+    public int getId()
+    {
+
+    }
+
+    public Calendar getDate()
+    {
+
+    }
+
+    public Set<Contact> getContacts()
+    {
+
+    }
 }
