66b9045d27a2b3502f0e3d94765a6608522eedd4
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-02-14 16:43:12 +0000

    implemented futuremeeting

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -1,3 +1,12 @@
-public class FutureMeetingImpl
+import java.util.Set;
+
+/** A meeting to be held in the future -
+no methods here, just a naming class */
+
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting
 {
+       public FutureMeetingImpl(Set<Contact> set)
+       {
+           super(set);
+       }
 }
