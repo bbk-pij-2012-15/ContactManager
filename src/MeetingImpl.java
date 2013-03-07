95735a4b3063c4a012b3ee72e5505efc4e2bdb95
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-07 01:18:47 +0000

    made meetingimpl implement serializable

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -1,6 +1,7 @@
+import java.io.Serializable;
 import java.util.*;
 
-public class MeetingImpl implements Meeting
+public class MeetingImpl implements Meeting, Serializable
 {
     private int meetingId;
     private Set<Contact> contactsAtMeeting = new HashSet<Contact>();
