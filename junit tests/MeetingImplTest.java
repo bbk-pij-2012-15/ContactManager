782da3a86f5c918d918cd52d0be6df08fa9e3c57
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-09 15:24:44 +0000

    first commit of new MeetingImpl test

---
+++ junit tests/MeetingImplTest.java
@@ -0,0 +1,84 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MeetingImplTest
+{
+    @Before
+    public void setUpTest()
+    {
+
+    }
+
+    @After
+    public void cleanUp()
+    {
+
+    }
+
+    @Test
+    public void testGetId()
+    {
+
+    }
+
+    @Test
+    public void testGetDate()
+    {
+
+    }
+
+    @Test
+    public void testGetNotes()
+    {
+
+    }
+
+    @Test
+    public void testGetContacts()
+    {
+
+    }
+
+    @Test
+    public void testGetSetInfo()
+    {
+
+    }
+
+    @Test
+    public void testGetMeetingInfo()
+    {
+
+    }
+
+    @Test
+    public void testGetFormattedDate()
+    {
+
+    }
+
+    @Test
+    public void testInPast()
+    {
+
+    }
+
+    @Test
+    public void testInFuture()
+    {
+
+    }
+
+    @Test
+    public void testAddNotes()
+    {
+
+    }
+
+    @Test
+    public void testReturnMeeting()
+    {
+
+    }
+}
