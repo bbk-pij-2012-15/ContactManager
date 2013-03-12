15afc4f55577fe4cfa8ae01134b2ad5648ce9753
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-12 13:52:33 +0000

    made imports explicit

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -1,5 +1,11 @@
 import java.io.Serializable;
-import java.util.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Comparator;
+
 
 public class MeetingImpl implements Meeting, Serializable
 {
