eb4d630c6c9911502ca9251199d12cf102fcd048
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-12 13:12:25 +0000

    made implement serializable just to be explicit (i know it's not needed), added comment

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -1,13 +1,15 @@
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 
 /** A meeting to be held in the future -
 no methods here, just a naming class */
 
-public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable
 {
        public FutureMeetingImpl(int id, Set<Contact> set, Calendar date)
        {
+           /** calls constructor in MeetingImpl */
            super(id, set, date);
        }
 }
