a8644c57f31cb3d58503d6ae3f51001fcf04fbc8
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-12 13:01:31 +0000

    added comments; made non-interface method protected

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -3,7 +3,8 @@ import java.io.Serializable;
 public class ContactImpl implements Contact, Serializable
 {
     private String name;
-    private String notes = "";
+    private String notes = "";   // initialized to empty string, so will never be returned null
+
     /** @param Id a unique id calculated by ContactManagerImpl
      (by adding 1 to the current size of the set of contacts) */
     private int Id;
@@ -19,27 +20,31 @@ public class ContactImpl implements Contact, Serializable
 
     public int getId()
     {
+        /** @return the unique id of the contact */
         return this.Id;
     }
 
     public String getName()
     {
+        /** @return the full name of the contact */
         return this.name;
     }
 
     public String getNotes()
     {
+        /** @return any and all notes associated with the contact.
+         *  If there are no notes, the empty string is returned */
         return this.notes;
     }
 
     public void addNotes(String note)
     {
-        /** @return prints a newline at the end of each added note and a dash
-        at the start so the list of notes remains clear to read */
+        /** method prints a newline at the end of each added note and a dash-bullet
+        *   at the start so the list of notes remains clear to read */
         notes += ("-" + note + "\n");
     }
 
-    public String getInfo()
+    protected String getInfo()
     {
         /** @return returns a useful String containing a Contact's name, id and any notes
         Will be used for test purposes and to display information about a specific Contact */
