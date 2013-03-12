5e66828c8266f185e022e7793cbc03f9d7b9ef91
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-12 13:12:50 +0000

    saved a line

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -2,8 +2,7 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 
-/** A meeting to be held in the future -
-no methods here, just a naming class */
+/** A meeting to be held in the future - no methods here, just a naming class */
 
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable
 {
