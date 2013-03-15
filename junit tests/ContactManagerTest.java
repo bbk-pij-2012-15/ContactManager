f29e8277c9aacd3aeb78b65df94b4ac4137a5868
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-15 13:12:37 +0000

    created test to test the interface

---
+++ junit tests/ContactManagerTest.java
@@ -0,0 +1,96 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContactManagerTest
+{
+    @Before
+    public void setUpTest()
+    {
+
+    }
+
+    @After
+    public void cleanUp()
+    {
+
+    }
+
+    @Test
+    public void testAddFutureMeeting()
+    {
+
+    }
+
+    @Test
+    public void testGetPastMeeting()
+    {
+
+    }
+
+    @Test
+    public void testGetFutureMeeting()
+    {
+
+    }
+
+    @Test
+    public void testGetMeeting()
+    {
+
+    }
+
+    @Test
+    public void testGetFutureMeetingListContact()
+    {
+
+    }
+
+    @Test
+    public void testGetFutureMeetingListCal()
+    {
+
+    }
+
+    @Test
+    public void testGetPastMeetingList()
+    {
+
+    }
+
+    @Test
+    public void testAddNewPastMeeting()
+    {
+
+    }
+
+    @Test
+    public void testAddMeetingNotes()
+    {
+
+    }
+
+    @Test
+    public void testAddNewContact()
+    {
+
+    }
+
+    @Test
+    public void testGetContactsInt()
+    {
+
+    }
+
+    @Test
+    public void testGetContactsStr()
+    {
+
+    }
+
+    @Test
+    public void testFlush()
+    {
+
+    }
+}
