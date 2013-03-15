c85770b2fdba563e830ed04db5ae6c525b27cfba
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-15 13:30:46 +0000

    added @before, @after and some globals

--- junit tests/ContactManagerTest.java
+++ junit tests/ContactManagerTest.java
@@ -2,24 +2,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Set;
+
 public class ContactManagerTest
 {
+    ContactManager conman;
+    Set<Contact> cset;
+    File data = new File("./contacts.txt");
+
     @Before
     public void setUpTest()
     {
-
+        conman = new ContactManagerImpl();
+        Contact c1 = new ContactImpl("ann", "contact 1", 1);
+        Contact c2 = new ContactImpl("bob", "contact 2", 2);
+        Contact c3 = new ContactImpl("cal", "contact 3", 3);
+        cset.add(c1);cset.add(c2);cset.add(c3);
     }
 
     @After
     public void cleanUp()
     {
-
+        conman = null;
+        data.delete();
     }
 
     @Test
     public void testAddFutureMeeting()
     {
-
+         conman.addFutureMeeting()
     }
 
     @Test
