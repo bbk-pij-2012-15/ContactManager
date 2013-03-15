011a4ca779101e8a524f971a5f0af0076782e0ae
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2013-03-15 14:06:34 +0000

    tests on addFutureMeeting pass

--- junit tests/ContactManagerTest.java
+++ junit tests/ContactManagerTest.java
@@ -1,15 +1,19 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.Set;
+import java.io.PrintStream;
+import java.util.*;
 
 public class ContactManagerTest
 {
     ContactManager conman;
-    Set<Contact> cset;
+    Set<Contact> cset = new HashSet<Contact>();
     File data = new File("./contacts.txt");
+    Calendar calendar = new GregorianCalendar();
 
     @Before
     public void setUpTest()
@@ -18,7 +22,8 @@ public class ContactManagerTest
         Contact c1 = new ContactImpl("ann", "contact 1", 1);
         Contact c2 = new ContactImpl("bob", "contact 2", 2);
         Contact c3 = new ContactImpl("cal", "contact 3", 3);
-        cset.add(c1);cset.add(c2);cset.add(c3);
+        conman.addNewContact("ann", "contact 1");conman.addNewContact("bob", "contact 2");conman.addNewContact("cal", "contact 3");
+        cset = conman.getContacts(1,2,3);
     }
 
     @After
@@ -31,7 +36,21 @@ public class ContactManagerTest
     @Test
     public void testAddFutureMeeting()
     {
-         conman.addFutureMeeting()
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(barr);
+        PrintStream old = System.out;
+        System.setOut(ps);
+
+        calendar.add(Calendar.YEAR, 2);
+        conman.addFutureMeeting(cset, calendar);
+        String expected = "Success - Meeting Scheduled!\n";
+        String actual = (barr.toString());
+        assertEquals(expected, actual);
+        List<Meeting> list = conman.getFutureMeetingList(calendar);
+        assertTrue(list.size() == 1);
+
+        System.out.flush();
+        System.setOut(old);
     }
 
     @Test
